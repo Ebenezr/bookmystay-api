80e0d46bc9aeccf451c13c31ff681355074c71ca
Ebenezr <ebenezarbukosia@gmail.com>
2023-03-28 19:19:01 +0300

    REF

---
+++ prisma/migrations/20230328161848_add/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `size` on the `Bed` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Bed" DROP COLUMN "size";
