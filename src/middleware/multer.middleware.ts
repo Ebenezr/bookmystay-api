a51de027b1c12826ebd5c73f50c76ee10e325f5c
Ebenezr <ebenezarbukosia@gmail.com>
2023-05-04 16:51:51 +0300

    refactor multer midleware

--- src/middleware/multer.middleware.ts
+++ src/middleware/multer.middleware.ts
@@ -31,7 +31,7 @@ router.post(
 );
 
 router.get('/images/:imageName', (req: Request, res: Response) => {
-  res.sendFile(path.resolve(process.cwd(), 'uploads', req.params.imageName));
+  res.sendFile(path.resolve(process.cwd(), '../uploads', req.params.imageName));
 });
 
 export default router;
